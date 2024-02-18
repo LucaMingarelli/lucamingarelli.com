6a2700939a40aae12cfe0ce1948f09fcf946b7b5
Luca Mingarelli <lucamingarelli@me.com>
2024-02-18 19:41:35 +0000

    size chart change

--- Teaching/Optimal_Control/vis.js
+++ Teaching/Optimal_Control/vis.js
@@ -49,7 +49,7 @@ var PATH_WIDTH = 1.5
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
 width = 460 - margin.left - margin.right,
-height = 300 - margin.top - margin.bottom;
+height = 450 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
