7cf6b00282f27eea1c65425c8ad72be2cc2fa8cd
Luca Mingarelli <lucamingarelli@me.com>
2024-02-23 15:22:20 +0000

    edit vars

--- Teaching/Optimal_Control/dp0.js
+++ Teaching/Optimal_Control/dp0.js
@@ -130,14 +130,14 @@ svg_dp.selectAll(".tick text").remove(); // Remove tick-labels
 var DELTA_dp = 10-0.35
 // Generate data for grid points
 var data_grid = [];
-for (var x = 1; x < 4; x++) {
-    for (var t = 1; t <= 6; t++) {
-        data_grid.push({x: x, t: t-1});
+for (var x_ = 1; x_ < 4; x_++) {
+    for (var t_ = 1; t_ <= 6; t_++) {
+        data_grid.push({x: x_, t: t_-1});
     }
 }
-for (var x = 1; x < 4; x++) {
-    for (var t = 1+DELTA_dp; t <= 6+DELTA_dp; t++) {
-        data_grid.push({x: x, t: t-1});
+for (var x_ = 1; x_ < 4; x_++) {
+    for (var t_ = 1+DELTA_dp; t_ <= 6+DELTA_dp; t_++) {
+        data_grid.push({x: x_, t: t_-1});
     }
 }
 
@@ -151,44 +151,44 @@ svg_dp.selectAll(".point")
     .attr("cy", function(d) { return yScale(d.x); });
 
 
-var lines = [];
-for (var t = 0; t <= 4; t++) {
-    for (var x = 1; x <= 3; x++) {
-      var point1 = data_grid.find(d => d.t === t && d.x === x);
+var lines_dp = [];
+for (var t_ = 0; t_ <= 4; t_++) {
+    for (var x_ = 1; x_ <= 3; x_++) {
+      var point1 = data_grid.find(d => d.t === t_ && d.x === x);
       for (var x_nxt = 1; x_nxt <= 3; x_nxt++){
-        for (var t_nxt = t; t_nxt <= 4; t_nxt++){
-            var point2 = data_grid.find(d => d.t === t+1 && d.x === x_nxt);
-            if (point1 && point2 && (x===2|t>0)) {
-              lines.push([point1, point2]);
+        for (var t_nxt = t_; t_nxt <= 4; t_nxt++){
+            var point2 = data_grid.find(d => d.t === t_+1 && d.x === x_nxt);
+            if (point1 && point2 && (x_===2|t_>0)) {
+                lines_dp.push([point1, point2]);
             }
         }
       }
     }
 }
 
-for (var t = DELTA_dp; t <= 4+DELTA_dp; t++) {
-    for (var x = 1; x <= 3; x++) {
+for (var t_ = DELTA_dp; t_ <= 4+DELTA_dp; t_++) {
+    for (var x_ = 1; x_ <= 3; x_++) {
       var point1 = data_grid.find(d => d.t === t && d.x === x);
       for (var x_nxt = 1; x_nxt <= 3; x_nxt++){
-          var point2 = data_grid.find(d => d.t === t+1 && d.x === x_nxt);
-          if (point1 && point2 && (x===2|t>DELTA_dp)) {
-            lines.push([point1, point2]);
+          var point2 = data_grid.find(d => d.t === t_+1 && d.x === x_nxt);
+          if (point1 && point2 && (x_===2|t_>DELTA_dp)) {
+            lines_dp.push([point1, point2]);
           }
       }
     }
 }
 
 // Define line generator
-var line = d3.line()
+var line_dp = d3.line()
     .x(function(d) { return xScale(d.t); })
     .y(function(d) { return yScale(d.x); });
 
 // Draw lines
 svg_dp.selectAll(".line")
-    .data(lines)
+    .data(lines_dp)
     .enter().append("path")
     .attr("class", "line")
-    .attr("d", line)
+    .attr("d", line_dp)
     .style("stroke", "rgba(255,0,0,0.12)")
     .style("stroke-width", 1)
     .style("fill", "none");
