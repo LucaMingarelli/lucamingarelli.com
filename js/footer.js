34c358f833ea325e33705e13d4e265aed1b0b78e
Luca Mingarelli <lucamingarelli@me.com>
2024-03-05 17:44:00 +0000

    footer adjust with buy me a coffee

--- js/footer.js
+++ js/footer.js
@@ -18,7 +18,7 @@ document.write('<footer class="w3-container w3-padding-8 w3-dark-grey">\
         <p><a href="https://www.linkedin.com/in/lucamingarelli/">LinkedIn</a></br>\
           <a href="https://scholar.google.co.uk/citations?user=NCAmtwMAAAAJ&hl=en">GoogleScholar</a></br>\
           <a href="https://github.com/LucaMingarelli">GitHub</a></br>\
-          <a href="https://www.codewars.com/users/LucaMingarelli"><img src="https://www.codewars.com/users/LucaMingarelli/badges/micro" alt="CodeWars"></a></p>\
+          <a href="https://www.buymeacoffee.com/lucamingarelli" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>\
       </div>\
     </div>\
   </footer> ')
