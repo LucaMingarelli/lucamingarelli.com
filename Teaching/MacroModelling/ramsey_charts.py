fa31eefc4382403f0b132640483b97109da7d34d
Luca Mingarelli <lucamingarelli@me.com>
2024-03-25 22:39:07 +0000

    updated chart

--- Teaching/MacroModelling/ramsey_charts.py
+++ Teaching/MacroModelling/ramsey_charts.py
@@ -34,8 +34,8 @@ plt.streamplot(Km, Cm, U, V, color=(np.abs(U)+np.abs(V))**0.0001, linewidth=0.5,
                density=1
                )
 plt.plot(k, c, color='k')
-k_start = (A/(δ+ρ))**2/4
-plt.axvline(k_start, color='k', linewidth=1, ymin=0.04)
+k_star = (A/(δ+ρ))**2/4
+plt.axvline(k_star, color='k', linewidth=1, ymin=0.04)
 plt.gca().annotate(r"$\dot{c}=0$", xy=(0.135, 0.28), xytext=(0.135, 0.28),annotation_clip=False)
 plt.gca().annotate(r"$\dot{k}=0$", xy=(0.7, 0.15), xytext=(0.7, 0.15),annotation_clip=False)
 xlim = 0, k_max*1.025
@@ -48,8 +48,9 @@ plt.gca().annotate("", xy=(xlim[1], 0), xytext=(-0.001, 0), arrowprops=dict(arro
 plt.gca().annotate("", xy=(0, ylim[1]), xytext=(0, -0.002), arrowprops=dict(arrowstyle="-|>", lw=0.5, color='black'))
 plt.gca().annotate(r"$c$", xy=(-0.035, 0.27), xytext=(-0.035, 0.27),annotation_clip=False)
 plt.gca().annotate(r"$k$", xy=(1.115, -0.015), xytext=(1.115, -0.015),annotation_clip=False)
-plt.scatter(x=1, y=0, marker='o', color='red', zorder=5)
-plt.scatter(x=k_start, y=f(k_start) - (n+δ)*k_start, marker='o', color='red', zorder=5)
+plt.scatter(x=1, y=0, marker='o', color='black', zorder=5)
+plt.scatter(x=0, y=0, marker='o', color='black', zorder=5)
+plt.scatter(x=k_star, y=f(k_star) - (n+δ)*k_star, marker='o', color='green', zorder=5)
 plt.tight_layout()
 plt.savefig("Teaching/MacroModelling/ramsey_chart.svg", transparent=True)
 plt.show()
