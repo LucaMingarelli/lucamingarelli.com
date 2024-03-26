05c23705cb5c9efffe8c7ef9f94d80310979ac8f
Luca Mingarelli <lucamingarelli@me.com>
2024-03-26 00:46:29 +0000

    x

--- Teaching/MacroModelling/ramsey_charts.py
+++ Teaching/MacroModelling/ramsey_charts.py
@@ -30,8 +30,7 @@ U[np.abs(U)==np.inf] = np.nan
 
 plt.figure(figsize=(5.5, 3.5))
 plt.streamplot(Km, Cm, U, V, color=(np.abs(U)+np.abs(V))**0.0001, 
-            #    linewidth=0.5,
-               linewidth=(U**2+V**2)**(1/2)*4,
+               linewidth=(U**2+V**2)**(1/2)*4, # 0.5, 
                cmap='viridis_r', broken_streamlines=False,
                density=1
                )
