49572e0e8acca0e1bd274fcf808b5d4af1494bb1
Luca Mingarelli <lucamingarelli@me.com>
2024-03-27 17:58:10 +0000

    ogm

--- Teaching/MacroModelling/ogm.py
+++ Teaching/MacroModelling/ogm.py
@@ -6,16 +6,16 @@ plt.figure(figsize=(5, 3))
 y = 0.25
 for n, x in enumerate(range(3)):
     y = (n+2) * 0.25
-    plt.gca().annotate("", xy=(x+1.2, y), xytext=(x+0.2, y), arrowprops=dict(arrowstyle="|-|"))
-    plt.gca().annotate("", xy=(x+2.2, y), xytext=(x+1.2, y), arrowprops=dict(arrowstyle="|-|"))
+    plt.gca().annotate("", xy=(x+1.2, y), xytext=(x+0.2, y), arrowprops=dict(arrowstyle="|-|", color='green'))
+    plt.gca().annotate("", xy=(x+2.2, y), xytext=(x+1.2, y), arrowprops=dict(arrowstyle="|-|", color='red'))
     plt.gca().annotate("Work", xy=(x + 1.2, y), xytext=(x + 0.5, y+0.03))
-    plt.gca().annotate("Retire", xy=(x + 2.2, y), xytext=(x + 1.4, y + 0.03))
+    plt.gca().annotate("Retire", xy=(x + 2.2, y), xytext=(x + 1.5, y + 0.03))
 
-plt.gca().annotate("", xy=(1.2, 0.25), xytext=(0.2, 0.25), arrowprops=dict(arrowstyle="|-|", alpha=0.2))
-plt.gca().annotate("", xy=(0.2, 0.25), xytext=(0, 0.25), arrowprops=dict(arrowstyle="-",linestyle="dotted", alpha=0.2))
+plt.gca().annotate("", xy=(0.2, 0.25), xytext=(0, 0.25), arrowprops=dict(arrowstyle="-",linestyle="dotted", alpha=0.2, color='green'))
+plt.gca().annotate("", xy=(1.2, 0.25), xytext=(0.2, 0.25), arrowprops=dict(arrowstyle="|-|", alpha=0.2, color='red'))
 
-plt.gca().annotate("", xy=(4.2, 1.25), xytext=(3.2, 1.25), arrowprops=dict(arrowstyle="|-|", alpha=0.2))
-plt.gca().annotate("", xy=(4.4, 1.25), xytext=(4.2, 1.25), arrowprops=dict(arrowstyle="-",linestyle="dotted", alpha=0.2))
+plt.gca().annotate("", xy=(4.2, 1.25), xytext=(3.2, 1.25), arrowprops=dict(arrowstyle="|-|", alpha=0.2, color='green'))
+plt.gca().annotate("", xy=(4.4, 1.25), xytext=(4.2, 1.25), arrowprops=dict(arrowstyle="-",linestyle="dotted", alpha=0.2, color='red'))
 
 plt.xlim(0, 4.5)
 plt.ylim(0, 1.5)
@@ -31,6 +31,7 @@ plt.xlabel("Time")
 plt.ylabel("Generations")
 
 plt.tight_layout()
+
 plt.savefig("Teaching/MacroModelling/ogm.svg", transparent=True)
 plt.show()
 
