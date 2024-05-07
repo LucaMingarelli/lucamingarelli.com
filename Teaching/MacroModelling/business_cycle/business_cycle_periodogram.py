160ad5f004130c08fdd454707823624b446dd3dc
Luca Mingarelli <lucamingarelli@me.com>
2024-05-07 20:54:29 +0000

    adjusted spectrogram

--- Teaching/MacroModelling/business_cycle/business_cycle_periodogram.py
+++ Teaching/MacroModelling/business_cycle/business_cycle_periodogram.py
@@ -1,6 +1,7 @@
 from fredapi import Fred
 import pandas as pd, numpy as np
 from Teaching.MacroModelling.business_cycle.utils import *
+from Teaching.MacroModelling.business_cycle.utils import _TICKS
 from statsmodels.tsa.filters.hp_filter import hpfilter
 
 import seaborn as sns
@@ -15,18 +16,31 @@ fred = Fred(api_key='6d0d2962ffaa5bf7eb12a6ac666d4ff9')
 gdp = fred.get_series('GDP').dropna()
 gdp.index = pd.to_datetime(gdp.index)
 # gdp = gdp[gdp.index<'2016-01-01']
-gdp = hpfilter(np.log(gdp), 1600)[0]
+gdp_f = hpfilter(np.log(gdp), 1600)[0]
 
 unrate = fred.get_series('UNRATE')
 unrate.index = pd.to_datetime(unrate.index)
+unrate = unrate[unrate.index.isin(gdp.index)]
 # unrate = unrate[unrate.index<'2016-01-01']
+unrate_f = hpfilter(np.log(unrate), 1600)[0]
 
 
 
-sf, ssd = smooth_periodogram(unrate.values)
+# sf, ssd = smooth_periodogram(unrate.values)
 
-plot_periodogram(unrate.values, normalise=True)
-plot_periodogram(gdp.values, normalise=True)
+
+kwargs = dict(ticks=_TICKS[:5], normalise=True, ticks_multiplier=4,
+              k=11, _n=600, _ns=20)
+plot_periodogram(gdp_f.values, 
+                 label='GDP', color='k', **kwargs)
+plot_periodogram(hpfilter(np.log(unrate), 1600)[0].values, 
+                 label='Unemployment', **kwargs)
+
+plt.xlabel('Years')
+plt.ylim(0,1)
+plt.yticks([0,1], [0,1])
+plt.legend(loc='upper left', bbox_to_anchor=[0,0,0,1.1], frameon=False, ncol=2)
+plt.tight_layout(rect=(0,0,1,0.975))
 plt.savefig("Teaching/MacroModelling/BC_periodogram.svg", transparent=True)
 plt.show()
 plt.close()
--- Teaching/MacroModelling/business_cycle/utils.py
+++ Teaching/MacroModelling/business_cycle/utils.py
@@ -30,19 +30,30 @@ def smooth_periodogram(x, window='hamming', scaling='density', detrend='linear')
     smooth_freq, smooth_spectral_density = smooth_interp(freq, spectral_density)
     return smooth_freq, smooth_spectral_density
 
-def plot_periodogram(x, smooth=True, ticks=_TICKS[1:-2], window='hamming', scaling='density', detrend='linear', normalise=False):
+def plot_periodogram(x, smooth=True, ticks=_TICKS[1:-2], window='hamming',
+                     scaling='density', detrend='linear',
+                     normalise=False, ticks_multiplier=4,
+                     k=nkr, _n=200, _ns=2, label=None, color=None):
     freq, spectral_density = periodogram(x, window=window, scaling=scaling, detrend=detrend)
     if smooth:
-        freq, spectral_density = smooth_interp(freq, spectral_density)
+        freq, spectral_density = smooth_interp(freq, spectral_density, k=k, _n=_n, _ns=_ns)
 
     freq, spectral_density = freq[freq>0], spectral_density[freq>0]
     if normalise:
         spectral_density /= spectral_density.max()
-    plt.plot(1 / freq, spectral_density)
+    x, y = 1 / freq, spectral_density
+    mask = (x>ticks[0] * 4)&(x<ticks[-1]*ticks_multiplier)
+    x, y = x[mask], y[mask]
+    line = plt.plot(x, y, color=color, label=label)
+    line[0].set_clip_on(False)
     plt.xscale('log')
-    plt.xticks(ticks * 4, ticks)
-    plt.xlim(ticks[0] * 4, ticks[-1]* 4)
-    plt.ylim(0, spectral_density[(1 / freq) < 4 * ticks[-1]].max())
+    ticks = np.array(ticks)
+    plt.xticks(ticks * ticks_multiplier, ticks)
+    plt.xlim(ticks[0] * 4, ticks[-1]*ticks_multiplier)
+    plt.ylim(0, spectral_density[(1 / freq) < ticks_multiplier * ticks[-1]].max())
+
+
+
 
 
 
